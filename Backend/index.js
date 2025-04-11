97e27bbc55da6b6127569c644596a3bbc4d512e9
Vivek9988 <vivek19102004@gmail.com>
2025-04-11 11:50:34 +0530

    Connecting to backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
+require('dotenv').config()
+const { default: mongoose } = require('mongoose');
 
 const app = express();
-const PORT = 3000;
+
+
+
+(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/vivek`)
+        console.log("connected")
+        
+    } catch (error) {
+        console.log("ERROR : " , error)
+        
+    }
+
+})()
 
 app.get('/', (req, res) => {
     res.send('Hello World from Express!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running at http://localhost:${process.env.PORT}`);
 });
